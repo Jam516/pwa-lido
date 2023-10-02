b705f857c2c3db248b7b078ff45aa7a12029bc9a
Jam516 <kufuorkofi@gmail.com>
2023-10-02 14:22:51 +0100

    commit

--- app/layout.tsx
+++ app/layout.tsx
@@ -15,22 +15,22 @@ import {
   getDefaultConfig,
 } from "connectkit";
 
-export const metadata: Metadata = {
-  title: {
-    default: siteConfig.name,
-    template: `%s - ${siteConfig.name}`,
-  },
-  description: siteConfig.description,
-  themeColor: [
-    { media: "(prefers-color-scheme: light)", color: "white" },
-    { media: "(prefers-color-scheme: dark)", color: "black" },
-  ],
-  icons: {
-    icon: "/favicon.ico",
-    shortcut: "/favicon-16x16.png",
-    apple: "/apple-touch-icon.png",
-  },
-}
+// export const metadata: Metadata = {
+//   title: {
+//     default: siteConfig.name,
+//     template: `%s - ${siteConfig.name}`,
+//   },
+//   description: siteConfig.description,
+//   themeColor: [
+//     { media: "(prefers-color-scheme: light)", color: "white" },
+//     { media: "(prefers-color-scheme: dark)", color: "black" },
+//   ],
+//   icons: {
+//     icon: "/favicon.ico",
+//     shortcut: "/favicon-16x16.png",
+//     apple: "/apple-touch-icon.png",
+//   },
+// }
 
 const alchemyId = process.env.ALCHEMY_API_KEY;
 const walletConnectProjectId = process.env.WALLETCONNECT_PROJECT_ID;
